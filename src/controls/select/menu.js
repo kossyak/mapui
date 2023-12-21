ccb62d75871ee5b32e869c31fe456500691853e5
SizNi <wwwwwwwww@list.ru>
2023-12-21 19:51:16 +0300

    добавил график режим для примера

--- src/controls/select/menu.js
+++ src/controls/select/menu.js
@@ -30,7 +30,12 @@ export default {
     },
     {
       key: 'reg',
-      title: 'График режимных наблюдений'
+      title: 'График режимных наблюдений',
+      view: () => {
+        const img = document.createElement('img')
+        img.src = 'regime.png'
+        return img
+      }
     },
     {
       key: 'raz',
