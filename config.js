d281d35a41dd7732130a48e3915cfe638cf8ba39
SizNi <wwwwwwwww@list.ru>
2024-01-21 16:36:33 +0300

    some fixes

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const host = '' // 'http://127.0.0.1:8080'
+const host = 'http://127.0.0.1:8080' // 'http://127.0.0.1:8080'
 
 export default {
   host,
   projection: 'EPSG:4326', // Проекция отображения координат (WGS-84)
   services: {
-    export: () => 'http://localhost:1234/services/export/index.html',
-    choosen_chem: (selected) => host + `/base/api/chem/${selected}/choosen_table/`,
+    export: () => host + '/static/servises/export/index.html',
+    choosen_chem: (selected) => host + `/base/api/chem/${selected.map(e => e.nameGwk)}/choosen_table/`,
     reg: (selected) => host + '/base/api/regime/' + selected.pk + '/plot/',
     gis: (selected) => host + '/base/api/gph/' + selected.pk + '/list/',
     efw: (selected) => host + '/base/api/efw/' + selected.pk + '/list/',
--- src/controls/select/details/index.js
+++ src/controls/select/details/index.js
@@ -45,17 +45,17 @@ export default {
       }
     },
     {
-      key: 'efw',
+      key: 'ofp',
       title: 'Данные по ОФР',
       view: (selected, config) => {
-        return new MS({ url: config.services.ofp(selected) }).iframe
+        return new MS({ url: config.services.efw(selected) }).iframe
       }
     },
     {
-      key: 'chem',
+      key: 'him',
       title: 'Химический анализ',
       view: (selected, config) => {
-        return new MS({ url: config.services.him(selected) }).iframe
+        return new MS({ url: config.services.chem(selected) }).iframe
       }
     },
     {
