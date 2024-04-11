7b6300665f3a8d881fb4840b2f170b4dbfa54347
kossyak <akosyak@protonmail.com>
2024-04-11 19:10:50 +0400

    search

--- src/options/filterSearch.js
+++ src/options/filterSearch.js
@@ -2,5 +2,5 @@ export default [
   { title: 'Все', active: true },
   { title: 'Скважины', content_types: '29' },
   { title: 'Разрезы', content_types: '85' },
-  { title: 'Полигоны', content_types: '28,28,71,' }
+  { title: 'Полигоны', content_types: '28,28,71' }
 ]
