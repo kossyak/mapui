b13aae9bedec825fc91c04b5d339acc8ed0f176d
SizNi <wwwwwwwww@list.ru>
2024-07-08 15:08:47 +0300

    поправил конфиг

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const host = 'https://darcydb.ru' // 'http://127.0.0.1:8080' //
+const host = 'http://127.0.0.1:8080' // 'https://darcydb.ru' //
 
 const test = `https://storage.yandexcloud.net/s3-for-groundwater-db/doc_1139/anal_3_1.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJExq0CJ58qDw1lmNtJrHdq%2F20240421%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240421T120642Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=eabe02403a9a43e3b6bfddcd302878bf998788bbb4808e0919f9d79089d75abc`
 
@@ -7,9 +7,9 @@ export default {
   projection: 'EPSG:4326', // Проекция отображения координат (WGS-84)
   user: {},
   wellTypes: [], // in edit form
-  onmessage: () => {}, // in main.js,
-  searchResults: () => {}, // in global search and edit form
-  getFeatureById: () => {}, // in global search and edit form
+  onmessage: () => { }, // in main.js,
+  searchResults: () => { }, // in global search and edit form
+  getFeatureById: () => { }, // in global search and edit form
   services: {
     export: () => host + '/static/services/export/index.html', // http://localhost:1234/export/index.html',
     choosen_chem: (selected) => host + `/base/api/chem/${selected.map(e => e.id)}/choosen_table/`,
@@ -29,9 +29,9 @@ export default {
     wells: ({ id }) => host + '/base/api/intakes/' + id + '/wells/',
     deadlines: ({ id }) => host + '/base/api/intakes/' + id + '/deadlines/',
     withdrawal: ({ id }) => host + '/base/api/intakes/' + id + '/withdrawal/',
-    source_file: (selected) => 'https://openweathermap.org/weathermap?basemap=map&cities=false&layer=precipitation&lat=0&lon=0&zoom=2',
-    pois: (selected) => 'https://openweathermap.org/weathermap?basemap=map&cities=false&layer=precipitation&lat=0&lon=0&zoom=2',
-    map_ref: (selected) => 'https://openweathermap.org/weathermap?basemap=map&cities=false&layer=precipitation&lat=0&lon=0&zoom=2',
+    source_file: ({ id }) => host + '/base/api/sections/' + id + '/file/',
+    pois: ({ id }) => host + '/base/api/sections/' + id + '/wells/',
+    map_ref: ({ id }) => host + '/base/api/sections/' + id + '/mapref/',
   },
   store: {
     wellsJson: {
@@ -68,7 +68,7 @@ export default {
       label: 'section types'
     },
     type_sediment: {
-      get: host +  '/base/api/dictentities/?entity=22',
+      get: host + '/base/api/dictentities/?entity=22',
       label: 'sediment types'
     }
   },
